e77c79095fd44a65902a38b18a78c7c92d336426
Marcos Knudsen <marcosknu14@gmail.com>
2024-09-23 13:59:02 -0300

    BowlingGame return 10

--- typescript/src/marcos/BowlingGame.ts
+++ typescript/src/marcos/BowlingGame.ts
@@ -2,17 +2,33 @@ export class BowlingGame {
   constructor() {
     this.frames = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
     this.actualFrame = 0;
+    this.try = 0;
   }
 
   score: number;
   frames: number[];
   actualFrame: number;
+  try: number;
 
   roll(pins) {
     this.frames[this.actualFrame] += pins;
+
+    if (this.frames[this.actualFrame] === 10) {
+      this.resetFrame()
+    }
+
+    this.try++;
   }
 
   getScore() {
-    return this.frames[this.actualFrame];
+    return this.frames.reduce((a, b) => a + b, 0);
+  }
+
+  private resetFrame(){
+    this.actualFrame++;
+
+    if (this.try === 0) {
+
+    }
   }
 }
